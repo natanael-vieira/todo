26ecfb7385f6c053196f37aceb204307d648526d
Natanael Vieira <natanaelsales@live.com>
2022-08-19 08:00:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo
+ Criador de lista de afazeres
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const HomePage(
+        title: '',
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Text("Menu"),
+        title: Text("ToDo - List"),
+        actions: <Widget>[
+          Icon(Icons.plus_one),
+        ],
+      ),
+      body: Center(
+        child: Text("Olá Mundo!"),
+      ),
+    );
+  }
+}
